cddccda35f7465e6d476175af1b7d040b134e30c
Daniela Paz <danielalizama.a@gmail.com>
2024-11-25 15:55:13 -0600

    add proyect

--- src/secciones/Proyectos.vue
+++ src/secciones/Proyectos.vue
@@ -1,17 +1,17 @@
 <template>
   <section class="w-full pt-16 pb-32" id="proyectos">
     <div
-      class="container mx-auto bg-gradient-to-b from-rose-400/20 via-rose-100 to-transparent my-7 rounded-t-3xl"
+      class=" mx-auto bg-gradient-to-b from-rose-400/20 via-rose-100 to-transparent my-7 rounded-t-3xl"
     >
       <div class="text-right p-7">
         <h2 class="text-5xl">_Proyectos__ /></h2>
       </div>
       <!--Galeria Proyectos-->
       <div
-        class="flex flex-col justify-center gap-4 lg:flex-row lg:flex-ow p-7 mx-7"
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols- xl:grid-cols-4 p-7 "
       >
         <div>
-          <h3 class="mb-2 text-2xl">
+          <h3 class="mb-2 text-2xl text-center">
             LandingRetro
             <strong class="text-2xl font-bold text-rose-500">
               vueJS 3 - tailwind</strong
@@ -21,7 +21,7 @@
             class="mx-auto relative group overflow-hidden rounded-lg shadow-2xl bg-[url(https://i.ibb.co/XsnnXR8/Captura-de-pantalla-2024-10-23-a-la-s-9-07-55-p-m.png)] bg-cover w-[300px] h-[200px]"
           >
             <div
-              class="absolute inset-0 flex items-center justify-center text-white transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100"
+              class="absolute inset-0 flex flex-col items-center justify-center text-white transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100"
             >
               <a
                 class="text-xl tracking-wider text-white transition-all hover:text-rose-400"
@@ -29,11 +29,17 @@
                 target="_blank"
                 >ir al proyecto</a
               >
+              <a
+                class="text-xl tracking-wider text-white transition-all hover:text-rose-700"
+                href="https://github.com/Seabird15/LandingRetro"
+                target="_blank"
+                >Link Github</a
+              >
             </div>
           </div>
         </div>
         <div>
-          <h3 class="mb-2 text-2xl">
+          <h3 class="mb-2 text-2xl text-center">
             PokeApi
             <strong class="text-2xl font-bold text-rose-500">
               vueJS 3 - tailwind</strong
@@ -43,7 +49,7 @@
             class="mx-auto relative group overflow-hidden rounded-lg shadow-2xl bg-[url(https://i.ibb.co/N3FKxYn/Captura-de-pantalla-2024-10-23-a-la-s-9-25-20-p-m.png)] bg-cover w-[300px] h-[200px]"
           >
             <div
-              class="absolute inset-0 flex items-center justify-center text-white transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100"
+              class="absolute inset-0 flex-col flex items-center justify-center text-white transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100"
             >
               <a
                 class="text-xl tracking-wider text-white transition-all hover:text-rose-400"
@@ -51,11 +57,17 @@
                 target="_blank"
                 >ir al proyecto</a
               >
+              <a
+                class="text-xl tracking-wider text-white transition-all hover:text-rose-700"
+                href="https://github.com/Seabird15/PokeApi-Vue-3"
+                target="_blank"
+                >Link Github</a
+              >
             </div>
           </div>
         </div>
         <div>
-          <h3 class="mb-2 text-2xl">
+          <h3 class="mb-2 text-2xl text-center">
             GifApp
             <strong class="text-2xl font-bold text-rose-500"> ReactJS</strong>
           </h3>
@@ -63,13 +75,46 @@
             class="mx-auto relative group overflow-hidden rounded-lg shadow-2xl bg-[url(https://i.ibb.co/C21xNfq/Captura-de-pantalla-2024-10-23-a-la-s-9-33-27-p-m.png)] bg-cover w-[300px] h-[200px]"
           >
             <div
-              class="absolute inset-0 flex items-center justify-center text-white transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100"
+              class="absolute inset-0 flex flex-col items-center justify-center text-white transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100"
             >
               <a
                 class="text-xl tracking-wider text-white transition-all hover:text-rose-400"
                 href="https://beautiful-pie-3a95a7.netlify.app/"
                 target="_blank"
                 >ir al proyecto</a
+              >
+              <a
+                class="text-xl tracking-wider text-white transition-all hover:text-rose-700"
+                href="https://github.com/Seabird15/gifAppReact"
+                target="_blank"
+                >Link Github</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h3 class="mb-2 text-2xl text-center">
+            SpotifyAPI
+            <strong class="text-2xl font-bold text-rose-500">vueJS 3 - Tailwind - Api Spotify</strong>
+          </h3>
+          <div
+            class="mx-auto relative group overflow-hidden rounded-lg shadow-2xl bg-[url(https://i.ibb.co/yyM0h7g/spotifyapi.png)] bg-cover w-[300px] h-[200px]"
+          >
+            <div
+              class="absolute inset-0 flex flex-col items-center justify-center text-white transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 group-hover:opacity-100"
+            >
+              <a
+                class="text-xl tracking-wider text-white transition-all hover:text-rose-400"
+                href="https://spectacular-sprite-78fa4b.netlify.app/"
+                target="_blank"
+                >ir al proyecto</a
+              >
+              <a
+                class="text-xl tracking-wider text-white transition-all hover:text-rose-700"
+                href="https://github.com/Seabird15/spotifyApi"
+                target="_blank"
+                >Link Github</a
               >
             </div>
           </div>
